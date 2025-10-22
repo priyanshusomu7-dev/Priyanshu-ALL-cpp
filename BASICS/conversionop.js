211041605a4bbce7ce112cbbf9389e474bd62701
Priyanshu Raj <priyanshuraj@Priyanshus-MacBook-Air.local>
2025-10-22 22:49:44 +0530

    update

---
+++ BASICS/conversionop.js
@@ -0,0 +1,53 @@
+let score = true
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let num = Number(score)
+console.log(typeof num);
+console.log(num);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "Priyanshu"
+console.log(typeof isLoggedIn);
+console.log(typeof (isLoggedIn));
+
+let booleanLoggedIn = Boolean(isLoggedIn)
+console.log(typeof booleanLoggedIn);
+console.log(booleanLoggedIn)
+
+// 1 = true; 0 => false
+// "' => false
+// "
+// Priyanshu" => true
+
+let someNumber = 69
+let stringNumber = String (someNumber)
+console. log (stringNumber);
+console. log (typeof stringNumber);
+
+//                 <-  ********       OPERATIONS       **********  ->
+
+let value = 69
+let negValue = -value
+console.log(negValue);
+
+console.log(2**5);
+console.log(2%4);
+
+let str1 = "Priyanshu"
+let str2 = " Raj"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2 + 2);
+console.log(1 + "2");
+console.log(1 + 1 + "2");
+console.log(+true);
+console.log(+"");
+
+let num1,num2,num3
+num1 = num2 = num2 = 4;
