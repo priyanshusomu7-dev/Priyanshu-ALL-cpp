334557ea1c7ba022b29b7a69a7817f04e7290751
Priyanshu Raj <priyanshuraj@Priyanshus-MacBook-Air.local>
2025-10-23 00:05:19 +0530

    update

--- BASICS/Variables.js
+++ BASICS/Variables.js
@@ -13,6 +13,5 @@ because of issue in block scope and functional scope
 accountEmail ="priyashsa@gmail.com"
 accountPassword ="987654"
 accountCity = "Patna"
-accountState
 
 console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
