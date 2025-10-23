d9aaaa55e1f0a080e1184c7301f3efbfa124e2a5
Priyanshu Raj <priyanshuraj@Priyanshus-MacBook-M2.local>
2025-10-23 22:37:01 +0530

    update

--- BASICS/datatype.js
+++ BASICS/datatype.js
@@ -1,3 +1,6 @@
+// JavaScript is a dynamic programming language. 
+// It allows variables to change type at runtime and supports flexible object handling.
+
 "use strict"; // treat all JS code as newer version
 
 // alert(3+3) => We are using Node.js not Browser this will not work.
@@ -6,6 +9,7 @@ let age = 20
 let isLoggedIn = false
 let state = "Bihar"
 
+// PRIMITIVE DATATYPES :-
 // Boolean => true and false.
 // null => A special keyword denoting a null value. (Because JavaScript is case-sensitive, null is not the same as Null, NULL, or any other variant.)
 // undefined =>A top-level property whose value is not defined.
@@ -20,4 +24,18 @@ console.log(typeof undefined)
 console.log(typeof Boolean)
 console.log(typeof String)
 console.log(typeof Symbol)
+console.log(typeof BigInt)
+
+// REFRENCE (NON PRIMITIVE DATATYPES) :-
+// Objects 
+let myObj = {
+    name : "Priyanshu"
+}
+// Array =>
+const heroes = ["Shaktiman","Spiderman","Deadpool"];
+
+// Functions =>
+const myFunction = function(){
+    console.log("Hey I'm learning JavaScript.");
+}
 
